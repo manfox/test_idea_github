03284810c54896f50660e8dd7bf10d4a685b0930
“foxman” <1258917637@qq.com>
2021-09-13 16:09:24 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author foxman
+ * @Description TODO
+ * @date 2021/9/13 下午2:48
+ * @Copyright Gowlib. All rights reserved.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
